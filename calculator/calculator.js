0ffdc80502f4854d8c4e951c610c5b96388214ba
christina kaliora <develop@pirron.one>
2020-03-25 09:12:01 +0100

    added some test

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,14 @@
 module.exports = {
   add,
+  hello
 };
 
 function add() {
   return null;
 }
+
+function hello() {
+ return 'Hello world!';
+}
+  
   
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,4 +1,12 @@
-const { add } = require('./calculator.js');
+const { add, hello } = require('./calculator.js');
 
 // test away!
+describe('Hello world', () => {
+    it('should return Hello world!', () => {
+        // for toBe to work the function needs to return something
+        expect(hello()).toBe("Hello world!");
+        expect(hello()).not.toBe("Hello world");
+        expect(hello()).not.toBeUndefined();
+    });
+});
  
